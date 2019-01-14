002e34fb5cde8e0e83cecc1b7c6c75ec985a73fc
anujp41 <anuj.pant41@gmail.com>
2019-01-14 15:04:45 -0500

    updated routes.js

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { FruitCount } = require('./model');
 
 const getAllFruits = () => FruitCount.findAll({ order: [['id', 'ASC']] });
-
+const getById = id => FruitCount.findById(id);
 const returnAllFruits = res => getAllFruits().then(fruits => res.json(fruits));
 
 router.get('/', (req, res, next) => {
@@ -10,15 +10,21 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/add/:id', (req, res, next) => {
-  FruitCount.findById(req.params.id)
+  getById(req.params.id)
     .then(fruit => fruit.increment('count', { by: 1 }))
     .then(() => returnAllFruits(res))
     .catch(err => next(err));
 });
 
 router.get('/subtract/:id', (req, res, next) => {
-  FruitCount.findById(req.params.id)
+  getById(req.params.id)
     .then(fruit => fruit.decrement('count', { by: 1 }))
+    .then(() => returnAllFruits(res))
+    .catch(err => next(err));
+});
+
+router.post('/name/:id', (req, res, next) => {
+  FruitCount.update({ fruit: req.body.name }, { where: { id: req.params.id } })
     .then(() => returnAllFruits(res))
     .catch(err => next(err));
 });
