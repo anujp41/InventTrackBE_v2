4455199a3cd3102780b1119d9f35ed0280bd9802
anujp41 <anuj.pant41@gmail.com>
2019-01-15 17:52:16 -0500

    removed console.log from `seed/index.js` file

--- seed/index.js
+++ seed/index.js
@@ -2,7 +2,7 @@ const { db, Fruit, User, UserFruit } = require('../model/index');
 const UserSeed = require('./User');
 const FruitSeed = require('./Fruit');
 const UserFruitSeed = require('./UserFruit');
-console.log('here ', process.env);
+
 const seed = () => {
   console.log('Seeding database!');
   db.sync({ force: true })
